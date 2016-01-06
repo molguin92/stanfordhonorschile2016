46dbbac31e3aae5fd71a2257a318ffa4a211636d
Manuel Olguin <molguin@dcc.uchile.cl>
2016-01-06 20:16:55 -0300

    move files to gh-pages

--- gen_html.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILES=./twine/*.html
-rm -f entries.html
-for f in $FILES
-do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  printf "<a href='$f'>$f</a>\n" >> entries.html 
-done
